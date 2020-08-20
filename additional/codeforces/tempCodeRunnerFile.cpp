4f06ae6c906b730ac5b785ce03fa9994f4e674af
Krithik13 <kirthikkirthik@gmail.com>
2020-08-20 20:19:44 +0530

    first

--- additional/codeforces/tempCodeRunnerFile.cpp
+++ additional/codeforces/tempCodeRunnerFile.cpp
@@ -1,21 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-   vector<char>p;
-   for(int i=0;i<4;i++){
-      char a;
-      cin>>a;
-      p.push_back(a);
-   }
-   for(auto i:p){
-      cout<<i<<" "; 
-   }
- 
-     for(int i=0;i<p.size();i++){
-      v[i]=p[i];
-   }
-  cout<<v;
 
-
-
+return 0;
 }
