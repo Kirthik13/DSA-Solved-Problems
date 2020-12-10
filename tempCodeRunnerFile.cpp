dd550659d20f095380927390a65c0042fe9487f0
Krithik13 <kirthikkirthik@gmail.com>
2020-12-10 21:52:40 +0530

    first commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,28 @@
-  for(auto i:v){
-      cout<<i;
-  }
+int k{};
+// for(int i=1;i<=n;i++){
+//         int j=1;
+//     while(1){
+//         if(i<=5*j){
+//          k=abs(i-(5*j));
+//         }
+//         else{
+//      k=(5*j)-i;
+
+//         }
+//         if(k<=m and k>0){
+//         // deb2(i,k);
+//         int p=5*j;
+//         // deb(p);
+//             cnt++;
+//         }
+//         else{
+//             if(k>m){
+//             break;
+//             }
+          
+//         }
+//         j++;
+
+//     }
+// }
+      
